dc53510d77f59b6037e95fdab1e3fd8e638cfc0c
Ali Aljarrah <ali.jarrah.109@gmail.com>
2025-03-30 13:47:27 +0300

    Add projects page content

--- include/footer.php
+++ include/footer.php
@@ -44,7 +44,7 @@
             </div>
 
             <div class="col-lg-6 col-12 mx-auto mt-5 mt-lg-0 ps-lg-5">
-                <iframe class="google-map img-fluid h-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6652.750346403488!2d36.287702691960384!3d33.51763002549413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1518e703bd037f1d%3A0xe8bcdc5c749e2648!2z2KfZhNi02LnZhNin2YY!5e0!3m2!1sen!2s!4v1742981048859!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <!-- <iframe class="google-map img-fluid h-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6652.750346403488!2d36.287702691960384!3d33.51763002549413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1518e703bd037f1d%3A0xe8bcdc5c749e2648!2z2KfZhNi02LnZhNin2YY!5e0!3m2!1sen!2s!4v1742981048859!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
             </div>
 
         </div>
