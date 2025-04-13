b3be4cd2fc1a07aa6f48a5efa735bb85eb8ff7c0
Ali Aljarrah <ali.jarrah.109@gmail.com>
2025-04-13 15:55:42 +0300

    Change icons

--- helper/constant.php
+++ helper/constant.php
@@ -2,3 +2,5 @@
 
 define("captcha_siteKey", "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI");
 define("captcha_secretKey", "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe");
+define("PHONE_NUMBER", "+963938432407");
+define("EMAIL_ADDRESS", "info@yelladev.com");
