ede84f5e8548acba3cb56c2269f99ce2d4b4c3fe
Ali Aljarrah <ali.jarrah.109@gmail.com>
2025-04-13 16:19:54 +0300

    change keys

--- helper/constant.php
+++ helper/constant.php
@@ -1,6 +1,6 @@
 <?php 
 
-define("captcha_siteKey", "6Lcr3BYrAAAAAOqyphviSNGrS8fDvevgDnmD-yf1");
-define("captcha_secretKey", "6Lcr3BYrAAAAAAlTfQCrR8WhSMNU7AIhO99hL8jW");
+define("captcha_siteKey", "6Lde3hYrAAAAAEU5tB5pXclqZgQ1U6noaVygUc9a");
+define("captcha_secretKey", "6Lde3hYrAAAAANVM5AKOb8ilLNRZz_fYcGxFb0m9");
 define("PHONE_NUMBER", "+963938432407");
 define("EMAIL_ADDRESS", "info@yelladev.com");
