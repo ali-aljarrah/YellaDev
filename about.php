446d68dc8649a0af1d6ef9718bd4242f72982f54
Ali Aljarrah <ali.jarrah.109@gmail.com>
2025-04-18 19:41:48 +0300

    update footer

--- about.php
+++ about.php
@@ -29,7 +29,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-6 text-center mb-4 mb-lg-0 wow animate__animated <?php echo ($lang == "ar") ? 'animate__fadeInRight' : 'animate__fadeInLeft'; ?>" data-wow-duration="2s" data-wow-delay=".2s">
-                        <img class="img-fluid rounded-4" width="413" height="267" loading="lazy" src="/assets/imgs/about.jpg" alt="<?php getContent("about_yellaDev", "about"); ?>">
+                        <img class="img-fluid rounded-4" width="413" height="267" loading="lazy" src="/assets/imgs/about.webp" alt="<?php getContent("about_yellaDev", "about"); ?>">
                     </div>
                     
                     <div class="col-lg-6 wow animate__animated <?php echo ($lang == "ar") ? 'animate__fadeInLeft' : 'animate__fadeInRight'; ?>"  data-wow-duration="2s" data-wow-delay=".2s">
